8c55ec90105cdb1fbe47614c6ebd5b47c040efbb
Chudopony <chudopony1@gmail.com>
2024-11-01 16:53:47 +0300

    Add files via upload

---
+++ Task7.java
@@ -0,0 +1,10 @@
+public class Task7 {
+    public static void main(String args[]) {
+        int a = 1;
+   
+        if (a>=0){
+        System.out.print ("false");
+        }else {
+        System.out.print ("true");}
+    }
+}
