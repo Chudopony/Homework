0c98d3c4e450ed18c21b19cc35893f0ea0acb80a
Chudopony <chudopony1@gmail.com>
2024-11-01 15:16:57 +0300

    Add files via upload

---
+++ Task5.java
@@ -0,0 +1,10 @@
+public class Task5 {
+    public static void main(String args[]) {
+        int a = 1;
+        int b = 2;
+        if (a+b >=10&& a+b <= 20){
+        System.out.print ("true");
+        }else {
+        System.out.print ("false");}
+    }
+}
