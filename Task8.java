a855879163b00a8da63296ff8b6a5d6932f3ca8e
Chudopony <chudopony1@gmail.com>
2024-11-02 11:48:20 +0300

    Add files via upload

---
+++ Task8.java
@@ -0,0 +1,10 @@
+public class Task8 {
+    public static void main(String args[]) {
+    String a1 = "Люблю жаву";
+    int number = 5;
+        while (number <= 10) {
+            System.out.println(a1);
+          number++;
+        }
+    }
+}
