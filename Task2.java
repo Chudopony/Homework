8c1f0f6ef5d0f6be63300a9bb41dbbe425e11900
Chudopony <chudopony1@gmail.com>
2024-11-04 13:46:29 +0300

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+    public static void main (String []args) {
+        checkSumSign();
+    }
+    public static void checkSumSign() {
+        int a = 1;
+        int b = 2;
+        if (a+b >=0){
+        System.out.print ("Сумма положительная");
+        }else {
+        System.out.print ("Сумма отрицательная");}
+    }
+}
