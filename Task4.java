5d2c99fcd8171f8bd8977bb23ffc1c9005f47539
Chudopony <chudopony1@gmail.com>
2024-11-04 13:55:50 +0300

    Update and rename CompareNumbers.java to Task4.java

--- Task4.java
+++ Task4.java
@@ -1,5 +1,8 @@
-public class CompareNumbers {
-    public static void main(String args[]) {
+public class Task4 {
+    public static void main (String []args) {
+        compareNumbers();
+    }
+    public static void compareNumbers() {
         int a = 1;
         int b = 2;
         if (a>=b){
