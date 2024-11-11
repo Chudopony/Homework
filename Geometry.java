e33e4a58012270d2c60346447bf316aa6b6d74c0
Chudopony <chudopony1@gmail.com>
2024-11-11 21:50:56 +0300

    Add files via upload

---
+++ Geometry.java
@@ -0,0 +1,136 @@
+interface Shape {
+    double getPerimeter();
+
+    double getArea();
+
+    String getFillColor();
+
+    String getBorderColor();
+
+    default void printInfo() {
+        System.out.println("Периметр: " + getPerimeter());
+        System.out.println("Площадь: " + getArea());
+        System.out.println("Цвет заливки: " + getFillColor());
+        System.out.println("Цвет границы: " + getBorderColor());
+    }
+}
+
+class Circle implements Shape {
+    private double radius;
+    private String fillColor;
+    private String borderColor;
+
+    public Circle(double radius, String fillColor, String borderColor) {
+        this.radius = radius;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+}
+
+class Rectangle implements Shape {
+    private double width;
+    private double height;
+    private String fillColor;
+    private String borderColor;
+
+    public Rectangle(double width, double height, String fillColor, String borderColor) {
+        this.width = width;
+        this.height = height;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+}
+
+class Triangle implements Shape {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private String fillColor;
+    private String borderColor;
+
+    public Triangle(double sideA, double sideB, double sideC, String fillColor, String borderColor) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return sideA + sideB + sideC;
+    }
+
+    @Override
+    public double getArea() {
+        double s = getPerimeter() / 2; 
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+}
+
+public class Geometry {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5, "Красный", "Черный");
+        Shape rectangle = new Rectangle(4, 6, "Синий", "Зеленый");
+        Shape triangle = new Triangle(3, 4, 5, "Желтый", "Фиолетовый");
+
+        System.out.println("Круг:");
+        circle.printInfo();
+
+        System.out.println("\nПрямоугольник:");
+        rectangle.printInfo();
+
+        System.out.println("\nТреугольник:");
+        triangle.printInfo();
+    }
+}
