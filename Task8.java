b8c765aa39ae8fc9fe226d3b88f40905e5d2a657
Chudopony <chudopony1@gmail.com>
2024-11-13 15:49:54 +0300

    Task8.java

--- Task8.java
+++ Task8.java
@@ -1,10 +1,11 @@
 public class Task8 {
-    public static void main(String args[]) {
-    String a1 = "Люблю жаву";
-    int number = 5;
-        while (number <= 10) {
-            System.out.println(a1);
-          number++;
+    public static void printString(String text, int num) {
+        for (int i = 0; i < num; i++) {
+            System.out.println(text);
         }
+    }
+
+    public static void main(String[] args) {
+        printString("Люблю переделывать дз", 3); 
     }
 }
