baefdfd995fb6621832a55138529b31f75d1f91d
Chudopony <chudopony1@gmail.com>
2024-11-13 15:30:08 +0300

    Task6.java

--- Task6.java
+++ Task6.java
@@ -1,10 +1,14 @@
 public class Task6 {
-    public static void main(String args[]) {
-        int a = 1;
-   
-        if (a>=0){
-        System.out.print ("Положительное число");
-        }else {
-        System.out.print ("Отрицательное число");}
+    public static void checkNumber(int a) {
+        if (a >= 0) {
+            System.out.println("Положительное число");
+        } else {
+            System.out.println("Отрицательное число");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkNumber(6);
+        checkNumber(-6);
     }
 }
